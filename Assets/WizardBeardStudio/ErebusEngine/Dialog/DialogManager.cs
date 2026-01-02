391a553be2f1650974342f84681ad1e75b530869
wizardbeard <ramonjlongiii@gmail.com>
2026-01-02 00:47:35 -0800

    maint(ui): removing zombie code

--- Assets/WizardBeardStudio/ErebusEngine/Dialog/DialogManager.cs
+++ Assets/WizardBeardStudio/ErebusEngine/Dialog/DialogManager.cs
@@ -34,7 +34,6 @@ namespace WizardBeardStudio.ErebusEngine.Dialog
 
             BuildForestFromHierarchy();
             ChooseEntryNode();
-            // ShowPage(EntryNode);
             StartDialog();
         }
 
