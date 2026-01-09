92ba0a88550e864876fc20ad4cbcfe65f6b9af85
wizardbeard <ramonjlongiii@gmail.com>
2026-01-09 02:13:06 -0800

    maint(rules): removing unused assembly reference

--- Assets/WizardBeardStudio/ErebusEngine/Combat/CombatRulesDemo.cs
+++ Assets/WizardBeardStudio/ErebusEngine/Combat/CombatRulesDemo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using WizardBeardStudio.ErebusEngine.PlayerCharacter;
 using WizardBeardStudio.ErebusEngine.RulesEngine.Builder;
 using WizardBeardStudio.ErebusEngine.RulesEngine.Core;
 
